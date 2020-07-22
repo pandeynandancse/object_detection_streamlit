9ee38161b389f0b6d11a528c7dc7c0463eeddb3b
Nandan Pandey <pandeynandancse@gmail.com>
2020-07-22 23:40:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"nandanpandeysmvdu@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo “
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+“ > ~/.streamlit/config.toml
