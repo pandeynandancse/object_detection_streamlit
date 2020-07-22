86f687bac86417c500188cd4f267fe34a05e9ad1
root <root@localhost.localdomain>
2020-07-22 23:11:36 +0530

    added all impimp fil files

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import cv2
+from PIL import Image
+import numpy as np
+import os
+
+from const import CLASSES, COLORS
+from settings import DEFAULT_CONFIDENCE_THRESHOLD, DEMO_IMAGE, MODEL, PROTOTXT
+
+
+@st.cache
+def process_image(image):
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+    net.setInput(blob)
+    detections = net.forward()
+    return detections
+
+
+@st.cache
+def annotate_image(
+    image, detections, confidence_threshold=DEFAULT_CONFIDENCE_THRESHOLD
+):
+    # loop over the detections
+    (h, w) = image.shape[:2]
+    labels = []
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > confidence_threshold:
+            # extract the index of the class label from the `detections`,
+            # then compute the (x, y)-coordinates of the bounding box for
+            # the object
+            idx = int(detections[0, 0, i, 1])
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # display the prediction
+            label = f"{CLASSES[idx]}: {round(confidence * 100, 2)}%"
+            labels.append(label)
+            cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(
+                image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2
+            )
+    return image, labels
+
+
+st.title("Object detection with MobileNet ")
+img_file_buffer = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+# st.sidebar.image(img_file_buffer)
+# content_image = cv2.imread(img_file_buffer)
+
+confidence_threshold = st.sidebar.slider(
+    "Confidence threshold", 0.0, 1.0, DEFAULT_CONFIDENCE_THRESHOLD, 0.05
+)
+
+if img_file_buffer is not None:
+    image = np.array(Image.open(img_file_buffer))
+    st.sidebar.image(image=np.asarray(image), use_column_width=True, 
+    caption='content image', clamp=True, channels='RGB')
+
+else:
+    demo_image = DEMO_IMAGE
+    image = np.array(Image.open(demo_image))
+
+detections = process_image(image)
+image, labels = annotate_image(image, detections, confidence_threshold)
+
+st.image(
+    image, caption=f"Annotated Image", use_column_width=True,
+)
+
+st.write(labels)
